748096981a920c9d2c37c782e6adb611517b2240
Tomas Kratochvile <kratot17fel.cvut.cz>
2023-03-23 20:55:46 +0100

    test

--- hw01/main.cpp
+++ hw01/main.cpp
@@ -6,23 +6,20 @@
 #include "main.hpp"
 #include "parse.hpp"
 
-typedef struct // Default values
+struct // Default values
 {
     int min = -99;
     int max = 100;
     int width = 3;
     bool align = false; // left: false, right: true
-} Cfg;
+} myCfg;
 
-int chToInt(char input)
-{
+int chToInt(char input) {
     return input - 65;
 };
 
-int main()
-{
+int main() {
     // Variable for decoded config
-    Cfg myCfg;
     config_t config;
     std::string line;
     do // Parse the configuration
@@ -30,33 +27,31 @@ int main()
         std::getline(std::cin, line);
 
         config = getConfig(line);
-        if (config.valid)
-        {
+        if (config.valid) {
             if (config.type == "min")
                 myCfg.min = stoi(config.value);
             if (config.type == "max")
                 myCfg.max = stoi(config.value);
             if (config.type == "width")
                 myCfg.width = stoi(config.value);
-            if (config.type == "align")
+            if (config.type == "align") {
                 if (config.value == "right")
                     myCfg.align = true;
                 else
                     myCfg.align = false;
+            }
         }
     } while (config.valid);
 
-    std::vector<std::vector<int>> values;
+    std::vector <std::vector<int>> values;
 
-    while (std::getline(std::cin, line))
-    {
+    while (std::getline(std::cin, line)) {
         std::stringstream ss(line);
         std::vector<int> row;
         std::string cell;
         int number;
 
-        while (std::getline(ss, cell, ';'))
-        {
+        while (std::getline(ss, cell, ';')) {
             try // If loaded cell is number
             {
                 number = std::stoi(cell);
@@ -75,18 +70,16 @@ int main()
     std::cout << "config.min=" << myCfg.min << std::endl;
     std::cout << "config.max=" << myCfg.max << std::endl;
     std::cout << "config.width=" << myCfg.width << std::endl;
-    if (myCfg.align == true)
+    if (myCfg.align)
         std::cout << "config.align=right\n"
                   << std::endl;
-    if (myCfg.align == false)
+    if (!myCfg.align)
         std::cout << "config.align=left\n"
                   << std::endl;
 
     // Print table
-    for (std::size_t i = 0; i < values.size(); i++)
-    {
-        for (std::size_t j = 0; j < values[i].size(); j++)
-        {
+    for (std::size_t i = 0; i < values.size(); i++) {
+        for (std::size_t j = 0; j < values[i].size(); j++) {
             std::cout << values[i][j] << " ";
             // Tady budu resit sumy
         }
