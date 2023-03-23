3a136a2fc7a613f647c081cbd4fce28f2039bb7b
Kratochvile <117267535+kratot17@users.noreply.github.com>
2023-03-23 09:32:41 +0000

    input

--- cl05/01.cpp
+++ cl05/01.cpp
@@ -2,30 +2,33 @@
 #include <vector>
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
+#include <map>
+#include <functional>
 
 namespace table
 {
-    std::string line()
+    std::string line(int elCnt, int elWidth)
     {
         std::stringstream ss;
-        ss << "+" << std::string(10, '-')
+        ss << "+" << std::string(10, '-') << "+" << std::string(10, '-') << "+" << std::string(10, '-') << "+\n";
+        return ss.str();
     }
 }
 
-struct  Auto
+struct Auto
 {
     int motor;
     int dvere;
     std::string znacka;
 
-    Auto (const char * n, int a, int b): dvere(b){};
+    Auto(const char *n, int a, int b) : znacka(n), motor(a), dvere(b){};
 };
 
-
-// slo by pouzit na hw01
-void operator<< (std::ostream & proud, Auto * data)
+std::ostream &operator<<(std::ostream &proud, Auto *data)
 {
-    proud << "|" << std::setw(10) << data->znacka << "|" << std::setw(10) << data->motor << "|" << std::setw(10) << data->dvere << "| /n";
+    proud << "|" << std::setw(10) << data->znacka << "|" << std::setw(10) << data->motor << "|" << std::setw(10) << data->dvere << "|\n";
+    return proud;
 }
 
 int main()
@@ -34,15 +37,30 @@ int main()
 
     Auto a = Auto("Peugeot", 1000, 1);
 
-    bazar.push_back(new Auto("Skoda", 1600, 5));
-    bazar.push_back(new Auto("Skoda", 1600, 5));
-    bazar.push_back(new Auto("Skoda", 1600, 5));
     bazar.push_back(new Auto("Tatra", 2500, 2));
+    bazar.push_back(new Auto("Skoda", 1600, 3));
+    bazar.push_back(new Auto("Skoda", 1600, 8));
+    bazar.push_back(new Auto("Skoda", 1600, 5));
+
+    // std::string cmd;
+    // std::cout << "Jakym smerem chces tridit (asc|desc)\n";
+    // std::cin >> cmd;
+
+    std::map<std::string, std::function<bool(Auto *, Auto *)>> prikaz;
+
+    // prikaz["asc"] = [](Auto *a, Auto *b)
+    // { return a->znacka < b->znacka; };
+    // prikaz["desc"] = [](Auto *a, Auto *b)
+    // { return a->znacka < b->znacka; };
+
+    // std::sort(bazar.begin(), bazar.end(), prikaz[cmd]);
+
+    std::cout << table::line();
 
     for (auto i : bazar)
     {
         // std::cout << i->znacka << " " << i->motor << "\n";
-        std::cout <<i;
+        std::cout << i << table::line();
     }
 
     return 0;
--- hw01/main.cpp
+++ hw01/main.cpp
@@ -6,58 +6,119 @@
 #include "main.hpp"
 #include "parse.hpp"
 
-typedef struct { // default values
+namespace table
+{
+    std::string line()
+    {
+        std::stringstream ss;
+        ss << "+" << std::string(10, '-') << "+" << std::string(10, '-') << "+" << std::string(10, '-') << "+\n";
+        return ss.str();
+    }
+}
+
+std::ostream &operator<<(std::ostream &proud, Auto *data)
+{
+    proud << "|" << std::setw(10) << data->znacka << "|" << std::setw(10) << data->motor << "|" << std::setw(10) << data->dvere << "|\n";
+    return proud;
+}
+
+typedef struct // Default values
+{
     int min = -99;
     int max = 100;
     int width = 3;
     bool align = false; // left: false, right: true
-    bool valid = false;
-}Cfg;
+} Cfg;
 
-int main(){  
-    // variable for decoded config
+int chToInt(char input)
+{
+    return input - 65;
+};
+
+int main()
+{
+    // Variable for decoded config
     Cfg myCfg;
     config_t config;
     std::string line;
-    do{
+    do // Parse the configuration
+    {
         std::getline(std::cin, line);
 
         config = getConfig(line);
-        if(config.valid){
-            std::cout << config.value << std::endl;
+        if (config.valid)
+        {
+            if (config.type == "min")
+                myCfg.min = stoi(config.value);
+            if (config.type == "max")
+                myCfg.max = stoi(config.value);
+            if (config.type == "width")
+                myCfg.width = stoi(config.value);
+            if (config.type == "align")
+                if (config.value == "right")
+                    myCfg.align = true;
+                else
+                    myCfg.align = false;
         }
-    }
-    while(config.valid);
-    
+    } while (config.valid);
+
     std::vector<std::vector<int>> values;
 
-    while(std::getline(std::cin, line)){
+    while (std::getline(std::cin, line))
+    {
         std::stringstream ss(line);
         std::vector<int> row;
         std::string cell;
         int number;
 
-        while (std::getline(ss,cell, ';')) {
-            try {   // if loaded cell is number
+        while (std::getline(ss, cell, ';'))
+        {
+            try // If loaded cell is number
+            {
                 number = std::stoi(cell);
-				row.push_back(number);
+                row.push_back(number);
             }
-            catch(const std::exception& e){ // if there is a text (SUM?)
+            catch (const std::exception &e) // if there is a text (SUM?)
+            {
                 int sum = 1;
                 row.push_back(sum);
             }
-        }   
-        values.push_back(row);     
+        }
+        values.push_back(row);
     }
 
-    // print out the config - TODO
-    
-	// print table
-    for (std::size_t i = 0; i < values.size(); i++){
-        for (std::size_t j = 0; j < values[i].size(); j++){
+    // Print out the my configuration
+    std::cout << "config.min=" << myCfg.min << std::endl;
+    std::cout << "config.max=" << myCfg.max << std::endl;
+    std::cout << "config.width=" << myCfg.width << std::endl;
+    if (myCfg.align == true)
+        std::cout << "config.align=right\n"
+                  << std::endl;
+    if (myCfg.align == false)
+        std::cout << "config.align=left\n"
+                  << std::endl;
+
+    // Print table
+    for (std::size_t i = 0; i < values.size(); i++)
+    {
+        for (std::size_t j = 0; j < values[i].size(); j++)
+        {
             std::cout << values[i][j] << " ";
+            // Tady budu resit sumy
         }
         std::cout << std::endl;
     }
+
+    // std::cout << table::line();
+
+    for (auto i : values)
+    {
+        for (auto j : i)
+        {
+            // std::cout << i->znacka << " " << i->motor << "\n";
+            std::cout << j << table::line();
+        }
+    }
+
     return 0;
 }
