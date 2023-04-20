991e47f45c3baa53362bba6b105b25c02059f2da
Tomas Kratochvile <kratot17fel.cvut.cz>
2023-04-20 21:53:11 +0200

    hw03 working first pt

--- hw03/main.cpp
+++ hw03/main.cpp
@@ -6,7 +6,6 @@
 #include "main.hpp"
 
 Database::Database() {
-    // Database constructor
 }
 
 // add item to the database
@@ -14,9 +13,31 @@ void Database::add(Item *a) {
     db.push_back(a);
 }
 
-// definition of the sort function according to name
+void Database::sort(std::string sort_by, std::string sort_order) {
+    std::cout << "I want to sort by: " << sort_by << " Order will be: " << sort_order << std::endl;
+    bool change_was_made = true;
+    if (sort_by == "id") {}
+    else if (sort_by == "name") {
+        while (change_was_made) {
+            for (int i = 0; i < db.size() - 1; ++i) {
+                if (sort_name()) {}
+            }
+        }
+    }
+    else if (sort_by == "year") {}
+}
+
+// sorting functions
+bool sort_id::operator()(const Item *a, const Item *b) {
+    return a->name.compare(b->name);
+}
+
 bool sort_name::operator()(const Item *a, const Item *b) {
-    return true;
+    return a->name.compare(b->name);
+}
+
+bool sort_year::operator()(const Item *a, const Item *b) {
+    return a->name.compare(b->name);
 }
 
 // constructor of the Item class
@@ -28,11 +49,15 @@ Item::Item(std::string a, std::string b) {
 Item::~Item() {
 }
 
+// printing functions
 void print_header(std::string argument) {
     std::cout << "+" << std::string(58, '-') << "+\n";
-    std::cout << "| "
-              << std::setfill(' ') << std::left << std::setw(57) << argument
-              << "|\n";
+    std::cout << "| " << std::setfill(' ') << std::left << std::setw(57) << argument << "|\n";
+}
+
+void print_footer(std::string argument) {
+    std::cout << "| " << std::setfill(' ') << std::left << std::setw(57) << argument << "|\n";
+    std::cout << "+" << std::string(58, '-') << "+\n";
 }
 
 void Item::print_info() {}
@@ -51,6 +76,7 @@ void Journal::print_info() {
               << name << "|\n" << "|" << std::string(4, ' ') << "| " << std::setw(52) << line << "|" << std::endl;
 }
 
+// ID initializing functions
 void Item::set_id(int i) {}
 
 void Book::set_id(int i) {
@@ -61,7 +87,8 @@ void Journal::set_id(int i) {
     ID = i + 1;
 }
 
-bool Book::compare_argument(std::string argument) {
+// search functions
+bool Book::search(std::string argument) {
     std::stringstream ss;
     ss << year << ", " << name << ", " << author;
     if (ss.str().find(argument) != std::string::npos)
@@ -70,7 +97,7 @@ bool Book::compare_argument(std::string argument) {
         return false;
 }
 
-bool Journal::compare_argument(std::string argument) {
+bool Journal::search(std::string argument) {
     std::stringstream ss;
     ss << year << ", " << name << ", " << issue << ", " << volume;
     if (ss.str().find(argument) != std::string::npos)
@@ -79,6 +106,27 @@ bool Journal::compare_argument(std::string argument) {
         return false;
 }
 
+bool Item::is_id(int id) { return 0; }
+
+bool Book::is_id(int id) {
+    return (id == ID);
+}
+
+bool search(std::string key, std::string string[]) {
+    for (int i = 0; i < (int) string->size(); i++) {
+        if (string[i] == key) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool Journal::is_id(int id) {
+    return (id == ID);
+}
+
+// main functions
 void Database::list() {
     int elCount = 0;
     print_header("List of all records");
@@ -87,46 +135,50 @@ void Database::list() {
         elCount++;
     }
     std::cout << "+" << std::string(4, '-') << "+" << std::string(53, '-') << "+\n";
-    std::cout << "| "
-              << "Total: " << elCount << std::string(49, ' ') << "|" << std::endl;
-    std::cout << "+" << std::string(58, '-') << "+\n";
+    print_footer("Total: " + std::to_string(elCount));
 }
 
 void Database::find(const std::string &key) {
     print_header("Search for \"" + key + "\"");
     int elCount = 0;
     for (int i = 0; i < (int) db.size(); i++) {
-        if (db[i]->compare_argument(key)) {
+        if (db[i]->search(key)) {
             db[i]->print_info();
             elCount++;
         }
     }
     std::cout << "+" << std::string(4, '-') << "+" << std::string(53, '-') << "+\n";
-    std::cout << "| " << "Total: " << elCount << std::string(49, ' ') << "|" << std::endl;
-    std::cout << "+" << std::string(58, '-') << "+\n";
+    print_footer("Total: " + std::to_string(elCount));
 }
 
 void Database::erase(const std::string &key) {
-    std::cout << db.size() << std::endl;
+    bool item_erased;
+    do {
+        item_erased = false;
+        for (int i = 0; i < (int) db.size(); i++) {
+            if (db[i]->search(key)) {
+                db.erase(db.begin() + i);
+                item_erased = true;
+            } else {
+                continue;
+            }
+        }
+    } while (item_erased);
+}
+
+void Database::remove(const std::string &id) {
+    bool item_removed = false;
     for (int i = 0; i < (int) db.size(); i++) {
-        if (db[i]->compare_argument(key)) {
+        if (db[i]->is_id(std::stoi(id))) {
             db.erase(db.begin() + i);
+            item_removed = true;
         } else {
             continue;
         }
     }
-}
-
-void Database::remove(const std::string &id) {
-    print_header("ID = " + id + "is not in the database");
-    std::cout << "+" << std::string(58, '-') << "+\n";
-
-    int elCount = 0;
-    for (int i = 0; i < (int) db.size(); i++) {
-        if (db[i]->compare_argument(id)) {
-            db[i]->print_info();
-            elCount++;
-        }
+    if (!item_removed) { // if no item was removed
+        print_header("ID = " + id + " is not in the database");
+        std::cout << "+" << std::string(58, '-') << "+\n";
     }
 }
 
@@ -137,8 +189,9 @@ void Database::set_id() {
 }
 
 Database::~Database() {
-    for (auto i = db.begin(); i != db.end(); i++)
+    for (auto i = db.begin(); i != db.end(); i++) {
         delete *i;
+    }
     db.clear();
 }
 
@@ -151,12 +204,24 @@ int main() {
     db.add(new Book("Tajuplny ostrov", "Jules Verne", "1874"));
     db.add(new Book("Ocelove mesto", "Jules Verne", "1879"));
     db.set_id();
+
+    std::string commands_req_arg[3] = {"find", "erase", "remove"};
     std::string a;
     while (std::getline(std::cin, a)) {
         size_t position;
         if ((position = a.find(":")) == std::string::npos) {
             if (a == "list")
                 db.list();
+            else if (search(a, commands_req_arg)) {
+                print_header("Command \"" + a + "\" expects some argument");
+                std::cout << "+" << std::string(58, '-') << "+\n";
+            } else if (a == "sort") {
+                print_header("Unknown sorting order");
+                std::cout << "+" << std::string(58, '-') << "+\n";
+            } else {
+                print_header("Unknown command \"" + a + "\"");
+                std::cout << "+" << std::string(58, '-') << "+\n";
+            }
         } else {
             std::string command = a.substr(0, position);
             std::string argument = a.substr(position + 1);
@@ -166,11 +231,17 @@ int main() {
                 db.erase(argument);
             else if (command == "remove") {
                 db.remove(argument);
-            } else {
-                print_header("Unknown command \"delete\"");
-                std::cout << "+" << std::string(58, '-') << "+\n";
+            } else if (command == "sort") {
+                if ((position = argument.find(":")) == std::string::npos) {
+                    db.sort(argument, "asc"); // default sorting: ascending
+                } else {
+                    std::string sort_by = argument.substr(0, position);
+                    std::string sort_order = argument.substr(position + 1);
+                    db.sort(sort_by, sort_order);
+                }
             }
         }
     }
     return 0;
 }
+
--- hw03/main.hpp
+++ hw03/main.hpp
@@ -4,54 +4,80 @@
 #ifndef __MAIN_HPP__
 #define __MAIN_HPP__
 
+struct sort_id;
 struct sort_name;
+struct sort_year;
 
-class Item
-{
+class Item {
 protected:
     int ID;
 
 public:
     std::string name, year;
+
     Item(std::string a, std::string b);
 
     virtual ~Item();
+
     virtual void print_info() = 0;
-    virtual bool compare_argument(std::string argument) = 0;
+
+    virtual bool search(std::string argument) = 0;
+
+    virtual bool is_id(int id) = 0;
+
     virtual void set_id(int i) = 0;
 
+    friend sort_id;
     friend sort_name;
+    friend sort_year;
 };
 
-class Book : public Item
-{
+class Book : public Item {
 public:
     std::string author;
+
     Book(std::string name, std::string author, std::string year) : Item(name, year), author(author) {}
+
     void print_info() override;
-    bool compare_argument(std::string argument) override;
+
+    bool search(std::string argument) override;
+
+    bool is_id(int id) override;
+
     void set_id(int i) override;
 };
 
-class Journal : public Item
-{
+class Journal : public Item {
 public:
     std::string volume;
     std::string issue;
-    Journal(std::string name, std::string volume, std::string issue, std::string year) : Item(name, year), volume(volume), issue(issue) {}
+
+    Journal(std::string name, std::string volume, std::string issue, std::string year) : Item(name, year),
+                                                                                         volume(volume), issue(issue) {}
+
     void print_info() override;
-    bool compare_argument(std::string argument) override;
+
+    bool search(std::string argument) override;
+
+    bool is_id(int id) override;
+
     void set_id(int i) override;
 };
 
-// declaration of the sort function according to name
-struct sort_name
-{
+// declaration of the sorting functions according to name
+struct sort_id {
     inline bool operator()(const Item *a, const Item *b);
 };
 
-class Database
-{
+struct sort_name {
+    inline bool operator()(const Item *a, const Item *b);
+};
+
+struct sort_year {
+    inline bool operator()(const Item *a, const Item *b);
+};
+
+class Database {
 private:
     std::vector<Item *> db;
 
@@ -59,6 +85,8 @@ public:
     Database();
 
     void add(Item *a);
+    void sort(std::string sort_by, std::string sort_order);
+
     void set_id();
     void list();                        // declaration of list method
     void find(const std::string &key);  // declaration of find method
